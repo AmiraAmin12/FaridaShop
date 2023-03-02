c8f94cab9f05ae56481b7eaeb3e9f95eb01b7af9
Amira Amin <amira.amin.s.95@gmail.com>
2023-03-02 15:06:52 +0200

    edit layout

--- resources/views/website/layout.blade.php
+++ resources/views/website/layout.blade.php
@@ -14,26 +14,26 @@
 		<link rel="stylesheet" type="text/css" href="{{asset('assets/css/bootstrap.min.css')}}">
 
 		<!-- icon - css include -->
-		<link rel="stylesheet" type="text/css" href="assets/css/fontawesome.css">
+		<link rel="stylesheet" type="text/css" href="{{asset('assets/css/fontawesome.css')}}">
 
 		<!-- animation - css include -->
-		<link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+		<link rel="stylesheet" type="text/css" href="{{asset('assets/css/animate.css')}}">
 
 		<!-- nice select - css include -->
-		<link rel="stylesheet" type="text/css" href="assets/css/nice-select.css">
+		<link rel="stylesheet" type="text/css" href="{{asset('assets/css/nice-select.css')}}">
 
 		<!-- carousel - css include -->
-		<link rel="stylesheet" type="text/css" href="assets/css/slick.css">
-		<link rel="stylesheet" type="text/css" href="assets/css/slick-theme.css">
+		<link rel="stylesheet" type="text/css" href="{{asset('assets/css/slick.css')}}">
+		<link rel="stylesheet" type="text/css" href="{{asset('assets/css/slick-theme.css')}}">
 
 		<!-- popup images & videos - css include -->
-		<link rel="stylesheet" type="text/css" href="assets/css/magnific-popup.css">
+		<link rel="stylesheet" type="text/css" href="{{asset('assets/css/magnific-popup.css')}}">
 
 		<!-- jquery ui - css include -->
-		<link rel="stylesheet" type="text/css" href="assets/css/jquery-ui.css">
+		<link rel="stylesheet" type="text/css" href="{{asset('assets/css/jquery-ui.css')}}">
 
 		<!-- custom - css include -->
-		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+		<link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.css')}}">
 
 	</head>
 
@@ -82,39 +82,39 @@
 
 
 		<!-- fraimwork - jquery include -->
-		<script src="assets/js/jquery-3.5.1.min.js"></script>
-		<script src="assets/js/popper.min.js"></script>
-		<script src="assets/js/bootstrap.min.js"></script>
+		<script src="{{asset('assets/js/jquery-3.5.1.min.js')}}"></script>
+		<script src="{{asset('assets/js/popper.min.js')}}"></script>
+		<script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
 
 		<!-- mobile menu - jquery include -->
 		<script src="assets/js/mCustomScrollbar.js"></script>
 
 		<!-- animation - jquery include -->
-		<script src="assets/js/parallaxie.js"></script>
-		<script src="assets/js/wow.min.js"></script>
+		<script src="{{asset('assets/js/parallaxie.js')}}"></script>
+		<script src="{{ashset('assets/js/wow.min.js')}}"></script>
 
 		<!-- nice select - jquery include -->
-		<script src="assets/js/nice-select.min.js"></script>
+		<script src="{{asset('assets/js/nice-select.min.js')}}"></script>
 
 		<!-- carousel - jquery include -->
-		<script src="assets/js/slick.min.js"></script>
+		<script src="{{asset('assets/js/slick.min.js')}}"></script>
 
 		<!-- countdown timer - jquery include -->
-		<script src="assets/js/countdown.js"></script>
+		<script src="{{asset('assets/js/countdown.js')}}"></script>
 
 		<!-- popup images & videos - jquery include -->
-		<script src="assets/js/magnific-popup.min.js"></script>
+		<script src="{{asset('assets/js/magnific-popup.min.js')}}"></script>
 
 		<!-- filtering & masonry layout - jquery include -->
-		<script src="assets/js/isotope.pkgd.min.js"></script>
-		<script src="assets/js/masonry.pkgd.min.js"></script>
-		<script src="assets/js/imagesloaded.pkgd.min.js"></script>
+		<script src="{{asset('assets/js/isotope.pkgd.min.js')}}"></script>
+		<script src="{{asset('assets/js/masonry.pkgd.min.js')}}"></script>
+		<script src="{{asset('assets/js/imagesloaded.pkgd.min.js')}}"></script>
 
 		<!-- jquery ui - jquery include -->
-        <script src="assets/js/jquery-ui.js"></script>
+        <script src="{{asset('assets/js/jquery-ui.js')}}"></script>
 
 		<!-- custom - jquery include -->
-		<script src="assets/js/custom.js"></script>
+		<script src="{{asset('assets/js/custom.js')}}"></script>
 
 
 	</body>
