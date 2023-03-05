14660a782f6ebf4db247ee53bb7febd28a632831
Amira Amin <amira.amin.s.95@gmail.com>
2023-03-05 17:29:05 +0200

    add login page for admin

--- routes/dasboard.php
+++ routes/dasboard.php
@@ -7,3 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('dashboard.index');
 });
+
+Route::get('/login', function () {
+    return view('dashboard.login');
+});
