9ddf0a06991f5abdf028b15242f702c11d3d3520
Amira Amin <amira.amin.s.95@gmail.com>
2023-03-07 21:11:09 +0200

    create routes for categories controller

--- routes/dasboard.php
+++ routes/dasboard.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\dashboard\categoryController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -11,3 +12,18 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('dashboard.login');
 });
+
+// Route::get('/categories', [categoryController ::class, 'index']);
+
+// Route::get('/categories/show/{id}', [categoryController ::class, 'show']);
+
+// Route::get('/categories/create', [categoryController ::class, 'create']);
+
+// Route::post('/categories/store', [categoryController ::class, 'store']);
+
+// Route::get('/categories/edit/{id}', [categoryController ::class, 'edit']);
+
+// Route::post('/categories/update/{id}', [categoryController ::class, 'update']);
+
+// Route::post('/categories/destroy/{id}', [categoryController ::class, 'destroy']);
+Route:: resource('categories',categoryController ::class);
